7ecc12562d5bf547672b8cbf962f75f91ee6d056
pavel-romanov-91 <101830399+pavel-romanov-91@users.noreply.github.com>
2023-03-09 21:00:11 +0300

    Add project files.

---
+++ Modul_6_Entity_Framework_Core/UserContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace FirstEF6App
+{
+    class UserContext : DbContext
+    {
+        public UserContext()
+            : base("DbConnection")
+        { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
